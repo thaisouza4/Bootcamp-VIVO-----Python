6bd485fec3e21d4713b68a437d802d60dee00d35
Thais Souza <141175409+thaisouza4@users.noreply.github.com>
2024-04-16 22:00:12 -0300

    Add files via upload

---
+++ print_input.py
@@ -0,0 +1,12 @@
+nome1 = input('Informe o seu nome: ')
+idade = input('Agora, informe a sua idade: ')
+
+print(nome1, idade)
+print(nome1, idade, end='...\n')
+print(nome1, idade, sep='#')
+print(nome1, idade, sep='#', end='...\n')
+
+nome = 'Thais'
+sobrenome = 'Souza'
+
+print(nome, sobrenome, end='...\né inteligente.')
---
+++ variaveis_constantes.py
@@ -0,0 +1,12 @@
+nome = 'Thais'
+idade = 27
+
+nome, idade = 'Rafael', 30
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ['PB', 'PE', 'CE', 'SC', 'RN']
+
+print(BRAZILIAN_STATES)
